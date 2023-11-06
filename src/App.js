418d34b7dc8318768ccafbe57da7ab7d81fac6e9
SunkissedQueen <baxtercm@outlook.com>
2023-11-06 00:14:05 -0800

    mock data and react state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
+import mockUsers from './mockUsers';
+import mockBlockers from './mockBlockers';
+import mockSolutions from './mockSolutions';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import BlockerIndex from './pages/BlockerIndex';
+import BlockerShow from './pages/BlockerShow';
+import BlockerNew from './pages/BlockerNew';
+import BlockerEdit from './pages/BlockerEdit';
+import UserDashBoard from './pages/UserDashBoard';
 
 export default function App() {
+
+  const [currentUser, setCurrentUser] = useState(mockUsers[2]) 
+  const [blockers, setBlockers] = useState(mockBlockers) 
+  const [solutions, setSolutions] = useState(mockSolutions) 
+
+  console.log("currentUser: ", currentUser)
+  console.log("blockers: ", blockers)
+  console.log("solutions: ", solutions)
+
   return(
     <>
       <Header />
---
+++ src/mockSolutions.js
@@ -0,0 +1,29 @@
+const mockSolutions = [
+  {
+    id: 1,
+    blocker_id: 1,
+    user_id: 1,
+    description: 'Sweet potato pie. No explanation necessary.',
+    require_followup: false,
+    attempted: true,
+    acceptable: true
+  },{
+    id: 2,
+    blocker_id: 2,
+    user_id: 3,
+    description: 'A vest. Easier to remove if you are in a hot house.',
+    require_followup: false,
+    attempted: false,
+    acceptable: true
+  },{
+    id: 3,
+    blocker_id: 3,
+    user_id: 2,
+    description: 'With some time management, you can go to both.',
+    require_followup: false,
+    attempted: false,
+    acceptable: true
+  }
+]
+
+export default mockSolutions
